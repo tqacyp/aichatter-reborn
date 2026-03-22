a7a24ebbd7595ceac93b0a76ae0085f8d43e3baa
tqacyp <tqacyp@163.com>
2026-03-22 16:43:47 +0800

    更新用户消息初步

--- src/main.js
+++ src/main.js
@@ -1,5 +1,32 @@
 import './style.css'
 import './code-style.css'
-import { init } from './test-format.js'
+// import { init } from './test-format.js'
 
-init();
+/* DOM 对象 */
+const chatHistories = document.getElementById("chat-histories-container")
+const messageContainer = document.getElementById("message-container")
+const inputBox = document.getElementById("input-box")
+
+function addUserMessage(content) {
+    // 追加用户消息到message-container
+    const newUserMessageContainer = document.createElement('div')
+    newUserMessageContainer.className = 'user-message-container'
+    const newUserMessage = document.createElement('div')
+    const contentWithNewlines = content.replace(/\n/g,'<br>')
+    newUserMessage.innerHTML = contentWithNewlines
+    newUserMessage.className = 'user-message'
+    newUserMessageContainer.appendChild(newUserMessage)
+    messageContainer.appendChild(newUserMessageContainer)
+}
+
+inputBox.addEventListener('keydown',function(event){
+    // 发送用户消息
+    if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault()
+        const content = this.value.trim()
+        if (content) {
+            addUserMessage(content)
+            this.value = ''
+        }
+    }
+})
